8cb78ed7d83216022184ffed6dc883a6d58cab4e
Khalid <Khalid>
2024-12-15 22:04:30 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function moveDodgerLeft() {
+    let left = parseInt(dodger.style.left, 10);
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    let left = parseInt(dodger.style.left, 10);
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
